e803c7d7239d3384c773fc507c631e092250e379
Łukasz Zbrzeski <47522558+lukaszek209@users.noreply.github.com>
2019-09-09 14:25:18 +0200

    Added GUI and changed endpoints

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Warsaw weather application
 This application show weather for Warsaw city
 
-This application on heroku: https://weather-rest-client.herokuapp.com/weather
+This application on heroku: https://weather-rest-client.herokuapp.com
+
+## Endpoints
+
+* [/weather/now](https://weather-rest-client.herokuapp.com/weather/now) - displays live weather as GUI
+
+* [/weather/json](https://weather-rest-client.herokuapp.com/weather/json) - displays live weather as JSON
--- src/main/java/com/example/springbootwarsawweatherapp/controller/WarsawWeatherController.java
+++ src/main/java/com/example/springbootwarsawweatherapp/controller/WarsawWeatherController.java
@@ -1,18 +1,21 @@
 package com.example.springbootwarsawweatherapp.controller;
 
 import com.example.springbootwarsawweatherapp.model.WeatherApi;
+
+import com.vaadin.flow.component.html.Label;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/weather")
 public class WarsawWeatherController {
 
-    @GetMapping("/weather")
-    public WeatherApi warsawWeather() {
+    public ResponseEntity<WeatherApi> connectToApi() {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<WeatherApi> exchange = restTemplate.exchange(
                 "http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl/forecast?id=524901&APPID=9f1c9d5001a81c74d30f19a75218c891",
@@ -21,6 +24,34 @@ public class WarsawWeatherController {
                 WeatherApi.class
         );
 
-        return exchange.getBody();
+        return exchange;
     }
+
+    @GetMapping("/json")
+    public WeatherApi weatherJson() {
+        return connectToApi().getBody();
+    }
+
+    @GetMapping("/now")
+    public String weatherNow() {
+
+        Double tempC = connectToApi().getBody().getMain().getTemp() - 273.15;
+
+        Label label = new Label(
+                connectToApi().getBody().getName()
+                        + ", " + connectToApi().getBody().getSys().getCountry()
+                        + "\nTemperature: " + tempC.intValue() + "°C"
+                        + "\nPressure: " + connectToApi().getBody().getMain().getPressure() + " hPa"
+                        + "\n\nWind:"
+                        + "\n     Speed: " + connectToApi().getBody().getWind().getSpeed() + " m/s"
+                        + "\n     Degree: " + connectToApi().getBody().getWind().getDeg()
+                        + "\n\nLocation of city: "
+                        + "\n         Longitude: " + connectToApi().getBody().getCoord().getLon()
+                        + "\n         Latitude: " + connectToApi().getBody().getCoord().getLat()
+        );
+
+
+        return label.getText();
+    }
+
 }
